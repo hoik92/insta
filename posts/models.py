f96440e57ccd856b527ae95d860b43b3043e3e4a
Hoik Jang <hoik92@nate.com>
2019-04-11 06:35:45 +0000

    Post 모델에 image 속성 추가 및 migrate 완료

--- posts/models.py
+++ posts/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Post(models.Model):
     content = models.CharField(max_length=150)
+    image = models.ImageField(blank=True)
     
     def __str__(self):
         return f"{self.id}: {self.content}"
